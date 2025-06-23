353040bcb951c410284cc4db75b690205f186c66
saulramos2005 <Sauldra79@gmail.com>
2025-06-22 22:22:24 -0400

    favicon

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -8,9 +8,8 @@ import RecentOrders from "@/components/ecommerce/RecentOrders";
 import DemographicCard from "@/components/ecommerce/DemographicCard";
 
 export const metadata: Metadata = {
-  title:
-    "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
-  description: "This is Next.js Home for TailAdmin Dashboard Template",
+  title: "Cargo Track",
+  description: "Envíos Internacionales hasta la puerta de tu casa",
 };
 
 export default function Ecommerce() {
